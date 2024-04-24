3aeb06bd915b68e620cdd6b6ec68bbe6192f8f24
Mitch Hammond <mitch.b.hammond@gmail.com>
2024-04-24 09:50:26 -0600

    wip

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,10 @@
 // build your `Project` model here
+//const db = require('../../data/dbConfig)
+
+
+//async function getProjectById(){
+//}
+
+
+
+//module.exports = {}
--- api/server.js
+++ api/server.js
@@ -1,1 +1,18 @@
 // build your server here and require it from index.js
+const express =require("express")
+const projectRouter = require("./project/router")
+const resourceRouter = require("./resource/router")
+const server=express()
+
+server.use(express.json())
+
+
+
+server.use('/api/project', projectRouter)
+server.use('/api/resource', resourceRouter)
+
+server.use('*', (req, res) =>{
+    res.json({api: " up and running"})
+})
+
+module.exports = server
